c06d16c31d173fc1708d17f967743ab50096c191
Austin Herring <awh44@drexel.edu>
2014-05-04 16:57:10 -0400

    Changes to MultipleChoice
    Made it possible to put in a (theoretically) infinite number of choices
    when adding a multiple choice question rather than just one for every
    letter (26).

--- Survey/src/Questions/MultipleChoice.java
+++ Survey/src/Questions/MultipleChoice.java
@@ -22,7 +22,6 @@ public class MultipleChoice extends Question
 
     protected void addChoice(String key, String answer)
 	{
-    	System.out.println(key + " " + answer);
     	if (choices_ == null)
     	{
     		System.out.println("why's it null");
@@ -41,13 +40,30 @@ public class MultipleChoice extends Question
     protected void addChoices()
     {
     	InputOutput info_getter = new ConsoleInputOutput();
-        info_getter.putString("How many choices would you like for your question? (At least 1, but less than 27.)\n");
+        info_getter.putString("How many choices would you like for your question?\n");
         int num_choices = info_getter.getInt();
-        for (char letter = 'A'; letter < 'A' + num_choices; letter++)
+        int iter_num = -1;
+        char letter = 'A';
+        for (int i = 0; i < num_choices; i++)
         {
-        	info_getter.putString("What would you like choice " + letter + " to be?\n");
+        	if (i % 26 == 0)
+        	{
+        		iter_num++;
+        		letter = 'A';
+        	}
+        	String display_letter;
+        	if (iter_num == 0)
+        	{
+        		display_letter = String.valueOf(letter);
+        	}
+        	else
+        	{
+        		display_letter = String.valueOf(letter) + iter_num;
+        	}
+        	info_getter.putString("What would you like choice " + display_letter + " to be?\n");
         	String choice = info_getter.getString();
-        	addChoice(String.valueOf(letter), choice);
+        	addChoice(display_letter, choice);
+        	letter++;
         }
     }
 
