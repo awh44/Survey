bbca792f1b61f595869c8d0fbd10c2c95d0588f9
Austin Herring <awh44@drexel.edu>
2014-05-06 00:42:22 -0400

    More of the same as last commit
    In regards to display() and restructuring.

--- Survey/src/Responses/ShortAnswerResponse.java
+++ Survey/src/Responses/ShortAnswerResponse.java
@@ -8,28 +8,14 @@ public class ShortAnswerResponse extends Response
 {
 	private static final long serialVersionUID = 1L;
     int maxLength_;
-
+    
     public ShortAnswerResponse(int max, InputOutput in_out, int maxLength)
 	{
         super(max, in_out);
         maxLength_ = maxLength;
     }
-
-    void addAnswer(String answer)
-	{
-
-    }
-
-    public void display()
-	{
-
-    }
-
-    public boolean equals(Object o)
-	{
-        return true;
-    }
-
+    
+    @Override
     public void getResponseFromUser(Set<String> valid_responses)
 	{
     	for (int i = 0; i < choices_.length; i++)
@@ -38,6 +24,13 @@ public class ShortAnswerResponse extends Response
     	}
     }
 
+    @Override
+    public boolean equals(Object o)
+	{
+        return true;
+    }
+
+    @Override
     public int hashCode()
 	{
         return 1;
